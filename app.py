82aba61f95527f8c4cafac08d7026408f4e3b4f4
jessechen150 <jesse.b.chen@yale.edu>
2022-08-09 22:50:42 -0400

    add and remove tab buttons in toolbar

--- app.py
+++ app.py
@@ -49,32 +49,42 @@ class MainWindow(QMainWindow):
         self.setContextMenuPolicy(Qt.ContextMenuPolicy.PreventContextMenu)
 
         # Toolbar actions
-        add_button = QAction(QIcon("icons/plus.png"), "Add", self)
+        add_tab_button = QAction(QIcon("icons/plus.png"), "Add a new bucket list", self)
+        cut_tab_button = QAction(QIcon("icons/cross.png"), "Remove current bucket list", self)
+        add_item_button = QAction(QIcon("icons/pencil-plus.png"), "Add a new item", self)
+        cut_item_button = QAction(QIcon("icons/scissors.png"), "Remove current item", self)
         undo_button = QAction(QIcon("icons/undo.png"), "Undo", self)
         redo_button = QAction(QIcon("icons/redo.png"), "Redo", self)
-        cut_button = QAction(QIcon("icons/scissors.png"), "Cut", self)
         help_button = QAction(QIcon("icons/question.png"), "Help", self)
 
         # Action shortcuts
-        add_button.setShortcut(QKeySequence("Ctrl+a"))
+        add_tab_button.setShortcut(QKeySequence("Ctrl+t"))
+        cut_tab_button.setShortcut(QKeySequence("Ctrl+w"))
+        add_item_button.setShortcut(QKeySequence("Ctrl+a"))
+        cut_item_button.setShortcut(QKeySequence("Ctrl+x"))
         undo_button.setShortcut(QKeySequence("Ctrl+z"))
         redo_button.setShortcut(QKeySequence("Ctrl+y"))
-        cut_button.setShortcut(QKeySequence("Ctrl+x"))
         help_button.setShortcut(QKeySequence("Ctrl+h"))
 
         # Toolbar connects
-        add_button.triggered.connect(self.add)
+        add_tab_button.triggered.connect(self.add_tab)
+        cut_tab_button.triggered.connect(self.cut_tab)
+        add_item_button.triggered.connect(self.add_item)
+        cut_item_button.triggered.connect(self.cut_item)
         undo_button.triggered.connect(self.undo)
         redo_button.triggered.connect(self.redo)
-        cut_button.triggered.connect(self.cut)
         help_button.triggered.connect(self.show_help_window)
 
         # Add to toolbar
         toolbar.addSeparator()
-        toolbar.addAction(add_button)
+        toolbar.addAction(add_tab_button)
+        toolbar.addAction(cut_tab_button)
+        toolbar.addSeparator()
+        toolbar.addAction(add_item_button)
+        toolbar.addAction(cut_item_button)
+        toolbar.addSeparator()
         toolbar.addAction(undo_button)
         toolbar.addAction(redo_button)
-        toolbar.addAction(cut_button)
         toolbar.addAction(help_button)
         toolbar.setMovable(False)
 
@@ -97,6 +107,7 @@ class MainWindow(QMainWindow):
         self.lastModified = QLabel("")
 
         self.save_button = QPushButton("Save")
+        self.save_button.setEnabled(False)
         self.save_button.clicked.connect(self.inspector_save)
 
         self.inspector = QGroupBox("Inspector")
@@ -120,17 +131,17 @@ class MainWindow(QMainWindow):
         self.layout.setStretch(0, 2)
         self.layout.setStretch(1, 1)
 
-    def add(self):
+    def add_tab(self):
+        print("Add tab")
+
+    def cut_tab(self):
+        print("Cut tab")
+
+    def add_item(self):
         item = self.bucket.add_item("New item")
         self.listWidget.addItem(item.title)
 
-    def undo(self):
-        print("Undo")
-
-    def redo(self):
-        print("Redo")
-
-    def cut(self):
+    def cut_item(self):
         i = self.listWidget.currentRow()
         if i >= 0:
             self.bucket.remove_index(i)
@@ -143,6 +154,12 @@ class MainWindow(QMainWindow):
             self.itemStatus.clear()
             self.itemStatus.setEnabled(False)
             self.lastModified.setText("")
+
+    def undo(self):
+        print("Undo")
+
+    def redo(self):
+        print("Redo")
 
     def show_help_window(self):
         self.help_window = HelpWindow()
@@ -161,19 +178,22 @@ class MainWindow(QMainWindow):
         self.itemStatus.addItems(["Not Started", "In Progress", "Completed"])
         self.itemStatus.setCurrentIndex(self.bucket.items[i].status)
         self.lastModified.setText(self.bucket.items[i].last_modified)
+        self.save_button.setEnabled(True)
 
     def inspector_save(self):
         i = self.listWidget.currentRow()
         
-        self.bucket.items[i].set_title(self.titleEdit.text())
-        self.listWidget.currentItem().setText(self.titleEdit.text())
-        self.bucket.items[i].set_description(self.descriptionEdit.toPlainText())
-        self.bucket.items[i].set_status(self.itemStatus.currentIndex())
+        if i >= 0:
+            self.bucket.items[i].set_title(self.titleEdit.text())
+            self.listWidget.currentItem().setText(self.titleEdit.text())
+            self.bucket.items[i].set_description(self.descriptionEdit.toPlainText())
+            self.bucket.items[i].set_status(self.itemStatus.currentIndex())
 
-        self.lastModified.setText(self.bucket.items[i].last_modified)
+            self.lastModified.setText(self.bucket.items[i].last_modified)
+        else:
+            self.save_button.setEnabled(False)
         
         
-
 
 app = QApplication([])
 window = MainWindow()
